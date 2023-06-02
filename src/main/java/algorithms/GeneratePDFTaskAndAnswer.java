1e0a0326bdd6c21116ffdeed939e74e44822045e
admitriy1931 <82140063+admitriy1931@users.noreply.github.com>
2023-06-02 18:08:53 +0500

    and version

--- src/main/java/algorithms/GeneratePDFTaskAndAnswer.java
+++ src/main/java/algorithms/GeneratePDFTaskAndAnswer.java
@@ -12,21 +12,20 @@ import com.lowagie.text.Document;
 import com.lowagie.text.Paragraph;
 
 
-public class StringSubstringGenerator {
+public class GeneratePDFTaskAndAnswer {
     private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyz";
     private static final int MIN_LENGTH = 4;
     private static final int MAX_LENGTH = 5;
-    private static double ver2; //вероятность того, что генератор выдаст подстроку из строки
+    private static final double ver2 = 0.5; //вероятность того, что генератор выдаст подстроку из строки
     private static int wordCount; //колличество слов в генерируемом предложении
     private static int optionCount; //колличество вариантов
     static Map<String, String> stringAndSubstring = new HashMap<>();
-    public StringSubstringGenerator(int wordCount, int optionCount, double ver) {
+    public GeneratePDFTaskAndAnswer(int wordCount, int optionCount) {
         this.wordCount = wordCount;
         this.optionCount = optionCount;
-        this.ver2 = ver;
     }
 
-    public void func1() {
+    public void generateStringAndPattern() {
         for (int i = 0; i < optionCount; i++) {
             Random random = new Random();
             double randomNumber2 = random.nextDouble();
@@ -59,10 +58,58 @@ public class StringSubstringGenerator {
         }
         return true;
     }
-    public void printStringndSubstring2() throws IOException {
+    public static List<List<String>> createTable(List<String> result) {
+        List<List<String>> items = new ArrayList<>();
+
+        for (String s : result) {
+            String[] currentResult = s.trim().split("\\s{5}");
+            if (currentResult.length > 1) {
+                items.add(new ArrayList<>());
+                int lastIndex = items.size() - 1;
+                items.get(lastIndex).add(currentResult[0]);
+                items.get(lastIndex).add(currentResult[1]);
+                items.get(lastIndex).add("");
+            } else {
+                int lastIndex = items.size() - 1;
+                items.get(lastIndex).remove(2);
+                items.get(lastIndex).add(currentResult[0]);
+            }
+        }
+
+        return items;
+    }
+
+    public static List<List<String>> createPrefixTable(List<String> result) {
+        List<List<String>> items = new ArrayList<>();
+
+        String[] firstPrefixData = result.get(0).split("\\) ")[1].split("\\D+");
+        String[] rawSecondPrefixData = result.get(1).split("\\) ")[1].split("[^A-Za-z]+");
+        String[] secondPrefixData = new String[firstPrefixData.length];
+        secondPrefixData[0] = "";
+
+        int counter = 1;
+        for (int i = 1; i < firstPrefixData.length; i++) {
+            if (Objects.equals(firstPrefixData[i], "0")) {
+                secondPrefixData[i] = "";
+            } else {
+                secondPrefixData[i] = rawSecondPrefixData[counter];
+                counter++;
+            }
+        }
+
+        for (int i = 1; i < firstPrefixData.length; i++) {
+            items.add(new ArrayList<>());
+            items.get(i - 1).add(String.valueOf(i - 1));
+            items.get(i - 1).add(firstPrefixData[i]);
+            items.get(i - 1).add(secondPrefixData[i]);
+        }
+
+        return items;
+    }
+    public void generatePdfAnswer() throws IOException {
         int i = 0;
         Document document = new Document();
-        PdfWriter.getInstance(document, new FileOutputStream("output2.pdf"));
+        PdfWriter.getInstance(document, new FileOutputStream("src/main/resources/answer.pdf"));
         document.open();
         for (Map.Entry<String, String> entry : stringAndSubstring.entrySet()) {
 
@@ -84,34 +131,58 @@ public class StringSubstringGenerator {
             paragraph.add(keyChunk);
             paragraph.add(questionMarkChunk);
             document.add(paragraph);
-///////////////////////////////
-            System.out.println("str: "+ entry.getKey() + "sub: " + entry.getValue());
-            System.out.println(output);
+
+            List<List<String>> out1 = createPrefixTable(output);
+            List<String> out2 = new ArrayList<>(output);
+            out2.remove(0);
+            out2.remove(0);
+
+            List<List<String>> out22 = createTable(out2);
+            System.out.println(out1); //
+            System.out.println(out22); //
 
             PdfPTable table = new PdfPTable(3);
-            table.setWidthPercentage(100);
-            //PdfPCell cell = new PdfPCell(new Paragraph("Text"));
-            //table.addCell(cell);
+            table.setWidthPercentage(70);
+            table.addCell("Step");
+            table.addCell("Prefix - function");
+            table.addCell("Prefix = Suffix");
 
-            for (int j = 0; j < 5; j++) {
-                table.addCell("Str " + j + ", Col 1");
-                table.addCell("Str " + j + ", Col 2");
-                table.addCell("Str " + j + ", Col 3");
+            for (int j = 0; j < out1.size(); j++) {
+                for(int k = 0; k <out1.get(0).size(); k++ ){
+                    String a = out1.get(j).get(k);
+                    table.addCell(a);
+                }
             }
+            PdfPTable table2 = new PdfPTable(3);
+            table2.setWidthPercentage(70);
+            table2.addCell("Substring");
+            table2.addCell("Text");
+            table2.addCell("State");
 
+            for (int q = 0; q < out22.size(); q++) {
+                for(int w = 0; w <out22.get(0).size(); w++ ){
+                    String aa = out22.get(q).get(w);
+                    table2.addCell(aa);
+                }
+            }
+            Chunk pf = new Chunk(" Prefix - function ", FontFactory.getFont(FontFactory.HELVETICA));
+            document.add(pf);
             document.add(table);
-
+            Chunk pr = new Chunk(" Protocol ", FontFactory.getFont(FontFactory.HELVETICA));
+            document.add(pr);
+            document.add(table2);
+            Chunk sp = new Chunk("\n");
+            document.add(sp);
             i++;
         }
         document.close();
 
-
     }
 
-    public void printStringndSubstring() throws IOException {
+    public void generatePdfTask() throws IOException {
         int i = 0;
         Document document = new Document();
-        PdfWriter.getInstance(document, new FileOutputStream("output.pdf"));
+        PdfWriter.getInstance(document, new FileOutputStream("src/main/resources/task.pdf"));
         document.open();
         for (Map.Entry<String, String> entry : stringAndSubstring.entrySet()) {
             //String f = "Option " + i + " \n" + "Is a "  + entry.getValue() + " substring of a string " + entry.getKey() + " ?";
--- src/main/java/org/openjfx/Main.java
+++ src/main/java/org/openjfx/Main.java
@@ -1,21 +1,14 @@
 package org.openjfx;
 
 
-import algorithms.StringSubstringGenerator;
+import algorithms.GeneratePDFTaskAndAnswer;
 import com.itextpdf.text.DocumentException;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws DocumentException, IOException {
         App.main(args);
-//        StringSubstringGenerator a = new StringSubstringGenerator(2, 30, 0.7);
-//        a.func1();
-//        a.printStringndSubstring();
-//        a.printStringndSubstring2();
-
 
     }
 }
